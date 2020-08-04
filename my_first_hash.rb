d299849d94f202bcea4289ec6ecf2a63b9cb4db7
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-04 18:01:59 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,15 +1,14 @@
-def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-  
+def my_hash
+hash= {"name" => "Naeem", "age" => "23"
+}
 end
 
 
-def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+def shipping_manifest()
+ the_menifest= {"whale bone corsets" => 5,
+"porcelain vases" => 2,
+"oil paintings" => 3}
 
 end
 
@@ -19,8 +18,8 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+                 
+  shipping_manifest["oil paintings"]
 
 end
 
