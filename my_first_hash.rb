2f7b3af1e19292a8c267bdb91fcdc17634cc3de9
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-04 18:21:41 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -31,6 +31,7 @@ def adding
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
+                  ""
                  }
   # add 2 muskets to the shipping_manifest hash below
 shipping_manifest["muskets"]= "2"
