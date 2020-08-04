14cac308771b542b5a2e860d0fa7049cd7effa9e
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-04 18:03:38 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,7 +15,8 @@ end
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   shipping_manifest["oil paintings"]
-                 
+  shipping_manifest["whale bone corsets"]  
+  shipping_manifest["porcelain vases"]
  
 
 end
